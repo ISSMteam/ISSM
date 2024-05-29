c5bb291cd0744312936cf2df3633d4814477a8a9
Tidbit Software <34657535+TidbitSoftware@users.noreply.github.com>
2024-05-29 01:31:33 -0700

    CHG: Adjusted tolerances; added empty Model dirs to examples where needed

--- test/NightlyRun/test455.py
+++ test/NightlyRun/test455.py
@@ -22,7 +22,7 @@ for i in ['P1bubble', 'P1bubblecondensed', 'P1xP2', 'P2xP1', 'P2', 'P1xP3', 'P2x
     md.flowequation.fe_HO = i
     md = solve(md, 'Stressbalance')
     field_names = field_names + ['Vx' + i, 'Vy' + i, 'Vz' + i, 'Vel' + i, 'Pressure' + i]
-    field_tolerances = field_tolerances + [9e-08, 6e-08, 6e-08, 6e-08, 3e-13]
+    field_tolerances = field_tolerances + [1e-07, 6e-08, 6e-08, 6e-08, 3e-13]
     field_values = field_values + [md.results.StressbalanceSolution.Vx,
                                    md.results.StressbalanceSolution.Vy,
                                    md.results.StressbalanceSolution.Vz,
