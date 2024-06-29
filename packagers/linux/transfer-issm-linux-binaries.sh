a83fea6aa67e1cc7f27faca7d299c309031ee15c
Tidbit Software <34657535+TidbitSoftware@users.noreply.github.com>
2024-06-29 14:01:52 -0700

    CHG: Temporary fix for not being able to connect via port 22

--- packagers/linux/transfer-issm-linux-binaries.sh
+++ packagers/linux/transfer-issm-linux-binaries.sh
@@ -16,7 +16,7 @@
 
 # Transfer package to ISSM Web site
 echo "Transferring package to ISSM Web site"
-scp -i ~/.ssh/debian_12_vm-to-ross ${COMPRESSED_PKG} jenkins@ross.ics.uci.edu:/var/www/html/${COMPRESSED_PKG}
+scp -p 2222 -i ~/.ssh/debian_12_vm-to-ross ${COMPRESSED_PKG} jenkins@ross.ics.uci.edu:/var/www/html/${COMPRESSED_PKG} # TODO: Fix issue with connecting on port 22
 
 if [ $? -ne 0 ]; then
 	echo "Transfer failed! Verify connection then build this project again (with -t/--transferonly option to skip building and packaging)."
