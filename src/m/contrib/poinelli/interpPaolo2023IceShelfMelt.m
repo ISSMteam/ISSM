38759406335acc83cfb108612424aaf38a5294d2
MPoinelli <mattia.poinelli@gmail.com>
2024-07-24 14:43:38 -0700

    NEW: Add contrib/poinelli folder and include Paolo2023 interpolation script

---
+++ src/m/contrib/poinelli/interpPaolo2023IceShelfMelt.m
@@ -0,0 +1,5 @@
+function output = interpPaolo2023IceShelfMelt(X,Y,string)
+    %INTERPRIGNOTICESHELFMELT - interp melt rates from Rignot et al. 2013
+    %
+    %   Usage:
+    %      output = interpRignotIceShelfMelt(X,Y)
