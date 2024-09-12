99209c86aa4a51d432f8135befdea30dacca7d0d
MPoinelli <mattia.poinelli@gmail.com>
2024-09-12 12:02:41 -0700

    CHG: include temporal component in Paolo2023 melt rate

--- src/m/contrib/poinelli/interpPaolo2023IceShelfMelt.m
+++ src/m/contrib/poinelli/interpPaolo2023IceShelfMelt.m
@@ -1,4 +1,4 @@
-function melt_paolo_interp = interpPaolo2023IceShelfMelt(X,Y,varargin)
+function melt_paolo_interp = interpPaolo2023IceShelfMelt(X,Y,initialDate,finalDate)
     %interpPaolo2023IceShelfMelt - interp melt rates from Paolo et al. 2023
     %
     %   Usage:
@@ -20,15 +20,25 @@ switch (oshostname())
 		error('hostname not supported yet');
 end
 
-disp('--Load dataset')
-xdata = double(ncread(meltrate,'x'));
-ydata = double(ncread(meltrate,'y'));
-time = double(ncread(meltrate,'time'));
-%melt_paolo = double(ncread(meltrate,'melt'));
 
-% Define the reference date
-ref_date = datetime(1950, 1, 1);
+disp('--Load Paolo 2023 dataset')
+xdata   = double(ncread(meltrate,'x'));
+ydata   = double(ncread(meltrate,'y'));
+time    = double(ncread(meltrate,'time'));
+
+% Reference date 01-01-1950
+ref_date  = datetime(1950, 1, 1);
 time_datetime = ref_date + days(time);
+
+if initialDate < time_datetime(1) || finalDate > time_datetime(end)
+    error(['Initial date is out of bounds. Date needs to be between:',datestr(time_datetime(1)),'to',datestr(time_datetime(end))]);
+end
+
+% find dates in range and check if it not empty
+datesInRange = time_datetime(time_datetime >= initialDate & time_datetime <= finalDate);
+if isempty(datesInRange)
+    error(['Date range did not find any match, increase your timespan. Date needs to be between:',datestr(time_datetime(1)),'and',datestr(time_datetime(end))]);
+end
 
 offset = 2;
 xmin = min(X(:)); xmax = max(X(:));
@@ -45,15 +55,24 @@ if isempty(posy), posy = numel(ydata); end
 id1y = max(1,find(ydata <= ymax,1)-offset);
 id2y = min(numel(ydata),posy(end)+offset);
 
-melt_data = double(ncread(meltrate,'melt',[id1x id1y 1],[id2x-id1x+1 id2y-id1y+1 1],[1 1 1]));
+id_time = find(time_datetime >= initialDate & time_datetime <= finalDate);
+
+% read meltrate
+melt_data = double(ncread(meltrate,'melt',[id1x id1y 1],[id2x-id1x+1 id2y-id1y+1 104],[1 1 1]));
 melt_data= permute(melt_data,[2 1 3]);
 
+% focus on area to interpolate from
 xdata2 = xdata(id1x:id2x);
 ydata2 = ydata(id1y:id2y);
 ydata2 = flipud(ydata2);
 melt_data = flipud(melt_data);
 
-disp('--Interp dataset to grid')
-melt_paolo_interp = InterpFromGridToMesh(xdata2,ydata2,melt_data,X,Y,NaN);
+disp('--Interp Paolo 2023 dataset to grid')
+melt_paolo_interp = zeros(length(X),length(id_time));
+i = 0;
+for t = id_time(1):id_time(end)
+    i = i +1;
+    melt_paolo_interp(:,i) = InterpFromGridToMesh(xdata2,ydata2,melt_data(:,:,t),X,Y,NaN);
+end
 
 return
