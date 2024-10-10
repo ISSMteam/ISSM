4edb1210fd1ca8f5fba321e525a523435f18d76f
Tidbit Software <support@tidbitsoftware.com>
2024-10-10 10:01:43 -0700

    CHG: Older versions of PETSc use config/configure.py (may need to change others)

--- externalpackages/petsc/install-3.14-discover.sh
+++ externalpackages/petsc/install-3.14-discover.sh
@@ -25,7 +25,7 @@ rm -rf petsc-${VER}
 
 # Configure
 cd ${PETSC_DIR}
-./configure \
+./config/configure.py \
 	COPTFLAGS="-g -O3" CXXOPTFLAGS="-g -O3" FOPTFLAGS="-g -O3" \
 	--prefix="${PREFIX}" \
 	--PETSC_DIR="${PETSC_DIR}" \
