a2cee86a3134e55a892da054a6c6a55af60a7cd2
Tidbit Software <support@tidbitsoftware.com>
2024-12-19 19:01:33 -0800

    CHG: Adjusted tolerances

--- test/NightlyRun/test404.py
+++ test/NightlyRun/test404.py
@@ -18,7 +18,7 @@ md = solve(md, 'Stressbalance')
 
 #Fields and tolerances to track changes
 field_names = ['Vx', 'Vy', 'Vz', 'Vel', 'Pressure']
-field_tolerances = [1e-05, 6e-06, 2e-06, 1e-06, 8e-07]
+field_tolerances = [2e-05, 6e-06, 2e-06, 1e-06, 8e-07]
 field_values = [md.results.StressbalanceSolution.Vx,
                 md.results.StressbalanceSolution.Vy,
                 md.results.StressbalanceSolution.Vz,
