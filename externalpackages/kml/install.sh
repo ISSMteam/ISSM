c8b9a0941fc572130eae07f95bf6f166bbb9d1c3
mmorligh <mathieu.morlighem@gmail.com>
2025-04-01 14:47:00 -0400

    CHG: cleaning up external packages

--- externalpackages/kml/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -eu
-
-#Download from ISSM server
-$ISSM_DIR/scripts/DownloadExternalPackage.sh 'https://issm.ess.uci.edu/files/externalpackages/kml_shapefile.zip' 'kml_shapefile.zip'
