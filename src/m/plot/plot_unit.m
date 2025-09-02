2907a6f5dbc0346ae90ca5a6c2ad1d70eb8ac845
Tobias W. Harvey <f007fk5@amundsen.thayer.dartmouth.edu>
2025-09-02 14:08:25 -0400

    add vertex number to plots datatips

--- src/m/plot/plot_unit.m
+++ src/m/plot/plot_unit.m
@@ -62,8 +62,12 @@ switch datatype,
 			patch( 'Faces',[B C D],'Vertices', [x y z],'FaceVertexCData',data(:),'FaceColor','interp','EdgeColor',edgecolor);
 			patch( 'Faces',[C A D],'Vertices', [x y z],'FaceVertexCData',data(:),'FaceColor','interp','EdgeColor',edgecolor);
 		else
-			A=elements(:,1); B=elements(:,2); C=elements(:,3); 
-			patch( 'Faces', [A B C], 'Vertices', [x y z],'FaceVertexCData', data(:),'FaceColor','interp','EdgeColor',edgecolor);
+                        A=elements(:,1); B=elements(:,2); C=elements(:,3); 
+                        p = patch( 'Faces', [A B C], 'Vertices', [x y z],'FaceVertexCData', data(:), 'FaceColor','interp','EdgeColor',edgecolor);
+                        tip = datatip(p,x(1),y(1),'Visible','off');
+                        p.UserData = 1:length(x);
+                        row = dataTipTextRow('Vertex', 'UserData');
+                        p.DataTipTemplate.DataTipRows(3) = row;
 		end
 
 	%quiver plot
