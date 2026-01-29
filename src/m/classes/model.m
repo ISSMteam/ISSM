7d71c4527a8abde89f250974a85e12e301dfcd28
Tobias W. Harvey <f007fk5@amundsen.thayer.dartmouth.edu>
2026-01-29 15:18:41 -0500

    changed to specfically modify basalforcings

--- src/m/classes/model.m
+++ src/m/classes/model.m
@@ -682,17 +682,7 @@ classdef model
 							md2.(model_fields{i}).(object_fields{j})=field(pos_elem,:);
 						elseif (fieldsize(1)==numberofelements1+1)
 							md2.(model_fields{i}).(object_fields{j})=[field(pos_elem,:); field(end,:)];
-                        %3D cell array
-                        elseif strcmp(class(field),'cell') & ndims(field) == 3
-                    		new_cell = cell(fieldsize);
-                        	for k=1:fieldsize(3)
-                        		layer = field{k};
-                                extracted_layer = [layer(pos_node, :); layer(end, :)];
-                             	new_cell{k} = extracted_layer;
-                        	end
-                            md2.(model_fields{i}).(object_fields{j}) = new_cell;
-                        end
-					end
+                                        end
 				else
 					%size = number of nodes * n
 					if fieldsize(1)==numberofvertices1
@@ -715,6 +705,13 @@ classdef model
 			md2.mesh.numberofvertices=numberofvertices2;
 			md2.mesh.elements=elements_2;
 
+                        % Extract ISMIP6 basal tf field
+                        if isa(md1.basalforcings, 'basalforcingsismip6')
+                            for i=1:numel(md.basalforcings.tf)
+                                md2.basalforcings.tf{i} = [md1.basalforcings.tf{i}(pos_node); md1.basalforcings.tf{i}(end)];
+                            end
+                        end
+                        
 			%mesh.uppervertex mesh.lowervertex
 			if isa(md1.mesh,'mesh3dprisms'),
 				md2.mesh.uppervertex=md1.mesh.uppervertex(pos_node);
