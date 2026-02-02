855b62c5ffb32f8c3fde38016beea73ba6eb7a81
Tidbit Software <34657535+TidbitSoftware@users.noreply.github.com>
2026-02-01 21:19:15 -0800

    Testing fix for running checkfield on subclass members that are lists of instances

--- src/m/consistency/checkfield.py
+++ src/m/consistency/checkfield.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
-from re import findall, split
+from re import findall, match, split
 from pairoptions import pairoptions
 from operator import attrgetter
 import MatlabFuncs as m
@@ -40,23 +40,26 @@ def checkfield(md, *args):
     options = pairoptions(*args)
 
     #get field from model
+    field = None
     if options.exist('field'):
         field = options.getfieldvalue('field')
         fieldname = options.getfieldvalue('fieldname', 'no fieldname')
     else:
         fieldname = options.getfieldvalue('fieldname')
-        fieldprefix = split(r'\[(.*?)\]', fieldname)[0]
-        fieldindexes = findall(r'\[(.*?)\]', fieldname)
-        field = attrgetter(fieldprefix)(md)
-        for index in fieldindexes:
-            try:
-                field = field[index.strip("\'")]
-            except TypeError:
-                field = field[int(index)]  #looking for an index and not a key
-
-    # that works for py2
-    #        exec("field = md.{}".format(fieldname))
-    #        exec("field = md.{}".format(fieldname), namespace)
+        fieldnametokens = split(r'\.', fieldname)
+        for i in range(len(fieldnametokens)):
+            if match(r'(.*?)\[(.*?)\]', fieldnametokens[i]):
+                listname = split(r'\[(.*?)\]', fieldnametokens[i])[0]
+                listindex = int(findall(r'\[(.*?)\]', fieldnametokens[i])[0])
+                if field == None:
+                    field = attrgetter(listname)(md)[listindex]
+                else:
+                    field = attrgetter(listname)(field)[listindex]
+            else:
+                if field == None:
+                    field = attrgetter(fieldnametokens[i])(md)
+                else:
+                    field = attrgetter(fieldnametokens[i])(field)
 
     if isinstance(field, (bool, int, float)):
         field = np.array([field])
