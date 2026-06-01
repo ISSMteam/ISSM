2f1b973c673597f084a75284d0fba503f7136387
Ken Mankoff <ken.mankoff@nasa.gov>
2026-06-01 13:06:55 +0200

    issmversion.py: Fix so docstring is correct and prints version

--- src/m/dev/issmversion.py
+++ src/m/dev/issmversion.py
@@ -8,9 +8,13 @@ def issmversion():
             issmversion()
     """
 
-print(' ')
-print((IssmConfig('PACKAGE_NAME')[0] + ' Version ' + IssmConfig('PACKAGE_VERSION')[0]))
-print(('(website: ' + IssmConfig('PACKAGE_URL')[0] + ' contact: ' + IssmConfig('PACKAGE_BUGREPORT')[0] + ')'))
-print(' ')
-print(('Build date: ' + IssmConfig('PACKAGE_BUILD_DATE')[0]))
-print('Copyright (c) 2009-2026 California Institute of Technology (3-Clause BSD License)')
+    print(' ')
+    print((IssmConfig('PACKAGE_NAME')[0] + ' Version ' + IssmConfig('PACKAGE_VERSION')[0]))
+    print(('(website: ' + IssmConfig('PACKAGE_URL')[0] + ' contact: ' + IssmConfig('PACKAGE_BUGREPORT')[0] + ')'))
+    print(' ')
+    print(('Build date: ' + IssmConfig('PACKAGE_BUILD_DATE')[0]))
+    print('Copyright (c) 2009-2026 California Institute of Technology (3-Clause BSD License)')
+
+
+# print 1x when imported
+issmversion()
