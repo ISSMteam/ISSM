0665e563a9367c5f514b0becee2a287e04ed1920
Cheng Gong <enigne@gmail.com>
2026-07-22 15:03:54 +0800

    ADD: Tongji-HPC to the cluster

---
+++ src/m/classes/clusters/tjhpc.m
@@ -0,0 +1,175 @@
+%HPC (Tongji) cluster class definition
+%
+%   Usage:
+%      cluster=tjhpc();
+%      cluster=tjhpc('np',3);
+%      cluster=tjhpc('np',3,'login','username');
+
+classdef tjhpc
+	properties (SetAccess=public)
+		% {{{
+		name          = 'tj'
+		login         = '';
+		modules       = {'intel/oneapi/24.0', 'cmake/3.31.6', 'gcc/13.2.0'};
+		numnodes      = 1;
+		cpuspernode   = 16;
+		codepath      = '';
+		executionpath = '';
+		partition	  = 'intel';
+		interactive   = 0;
+		time          = 10; %in hours
+		memory        = 10;  %in Gb
+		email         = 'END,FAIL';
+		emailname  = '';
+		email_domain  = 'tongji.edu.cn';
+	end
+	%}}}
+	methods
+		function cluster=tjhpc(varargin) % {{{
+
+			%initialize cluster using default settings if provided
+			if (exist('tjhpc_settings')==2), tjhpc_settings; end
+
+			%use provided options to change fields
+			cluster=AssignObjectFields(pairoptions(varargin{:}),cluster);
+		end
+		%}}}
+		function disp(cluster) % {{{
+			%  display the object
+			disp(sprintf('class ''%s'' object ''%s'' = ',class(cluster),inputname(1)));
+			disp(sprintf('    name:  %s',cluster.name));
+			disp(sprintf('    login: %s',cluster.login));
+			disp(sprintf('    modules: %s',strjoin(cluster.modules,', ')));
+			disp(sprintf('    numnodes:    %i',cluster.numnodes));
+			disp(sprintf('    cpuspernode: %i',cluster.cpuspernode));
+			disp(sprintf('    time: %i hours',cluster.time));
+			disp(sprintf('    memory: %i Gb',cluster.memory));
+			disp(sprintf('    email: %s (notifications: BEGIN,END,FAIL)',cluster.email));
+			disp(sprintf('    codepath:      %s',cluster.codepath));
+			disp(sprintf('    executionpath: %s',cluster.executionpath));
+			disp(sprintf('    interactive: %i',cluster.interactive));
+		end
+		%}}}
+		function numprocs=nprocs(cluster) % {{{
+			%compute number of processors
+			numprocs=cluster.numnodes*cluster.cpuspernode;
+		end
+		%}}}
+		function md = checkconsistency(cluster,md,solution,analyses) % {{{
+			%Miscellaneous
+			if isempty(cluster.login), md = checkmessage(md,'login empty'); end
+			if isempty(cluster.codepath), md = checkmessage(md,'codepath empty'); end
+			if isempty(cluster.executionpath), md = checkmessage(md,'executionpath empty'); end
+		end
+		%}}}
+		function BuildKrigingQueueScript(cluster, md, filename) % {{{
+
+         %Get variables from md
+         dirname         = md.private.runtimename;
+         modelname       = md.miscellaneous.name;
+         solution        = md.private.solution;
+         io_gather       = md.settings.io_gather;
+
+         %checks
+			if(md.debug.valgrind) disp('valgrind not supported by cluster, ignoring...'); end
+			if(md.debug.gprof)    disp('gprof not supported by cluster, ignoring...'); end
+
+			%write queuing script 
+			fid=fopen(filename, 'w');
+			fprintf(fid,'#!/bin/bash\n');
+			fprintf(fid,'#SBATCH --job-name=%s\n',modelname);
+			fprintf(fid,'#SBATCH --partition=%s\n', partition);
+			fprintf(fid,'#SBATCH --output=%s.outlog \n',modelname);
+			fprintf(fid,'#SBATCH --error=%s.errlog \n',modelname);
+			fprintf(fid,'#SBATCH --time=%s\n',eraseBetween(datestr(cluster.time/24,'dd-HH:MM:SS'),1,1)); %walltime is in d-HH:MM:SS format. cluster.time is in hour
+			fprintf(fid,'#SBATCH --nodes=%i\n',cluster.numnodes);
+			fprintf(fid,'#SBATCH --ntasks=%i\n',cluster.nprocs());
+			fprintf(fid,'#SBATCH --cpus-per-task=1\n');
+			fprintf(fid,'#SBATCH --mem=%iG\n',cluster.memory);
+			if ~isempty(cluster.email)
+				fprintf(fid,'#SBATCH --mail-type=%s\n',cluster.email);
+				fprintf(fid,'#SBATCH --mail-user=%s@%s\n',cluster.emailname, cluster.email_domain);
+			end
+			fprintf(fid,'\n');
+         for i=1:numel(cluster.modules)
+            fprintf(fid,['module load ' cluster.modules{i} '\n']);
+         end
+
+			fprintf(fid,'export ISSM_DIR="%s/../"\n',cluster.codepath);
+			fprintf(fid,'source $ISSM_DIR/etc/environment.sh\n');      
+			fprintf(fid,'cd %s/%s\n\n',cluster.executionpath,dirname);
+			fprintf(fid,'srun %s/kriging.exe %s %s\n', cluster.codepath,[cluster.executionpath '/' modelname],modelname);
+			if ~io_gather, %concatenate the output files:
+				fprintf(fid,'cat %s.outbin.* > %s.outbin',modelname,modelname);
+			end
+			fclose(fid);
+		end
+		%}}}
+		function BuildQueueScript(cluster, md, filename, executable) % {{{
+
+         %Get variables from md
+         dirname         = md.private.runtimename;
+         modelname       = md.miscellaneous.name;
+         solution        = md.private.solution;
+         io_gather       = md.settings.io_gather;
+
+         %checks
+
+			if(md.debug.valgrind); disp('valgrind not supported by cluster, ignoring...'); end
+			if(md.debug.gprof);    disp('gprof not supported by cluster, ignoring...'); end
+
+			%write queuing script
+			fid=fopen(filename, 'w');
+			fprintf(fid,'#!/bin/bash -l\n');
+         fprintf(fid,'#SBATCH --job-name=%s\n',modelname);
+         fprintf(fid,'#SBATCH --partition=%s\n', cluster.partition);
+         fprintf(fid,'#SBATCH --output=%s.outlog \n',modelname);
+         fprintf(fid,'#SBATCH --error=%s.errlog \n',modelname);
+         fprintf(fid,'#SBATCH --time=%s\n',eraseBetween(datestr(cluster.time/24,'dd-HH:MM:SS'),1,1)); %walltime is in d-HH:MM:SS format. cluster.time is in hour
+         fprintf(fid,'#SBATCH --nodes=%i\n',cluster.numnodes);
+         fprintf(fid,'#SBATCH --ntasks=%i\n',cluster.nprocs());
+         fprintf(fid,'#SBATCH --cpus-per-task=1\n');
+         fprintf(fid,'#SBATCH --mem=%iG\n',cluster.memory);
+			if ~isempty(cluster.email)
+				fprintf(fid,'#SBATCH --mail-type=%s\n',cluster.email);
+				fprintf(fid,'#SBATCH --mail-user=%s@%s\n',cluster.emailname, cluster.email_domain);
+			end
+			fprintf(fid,'\n');
+			for i=1:numel(cluster.modules)
+            fprintf(fid,['module load ' cluster.modules{i} '\n']);
+         end
+			fprintf(fid,'\n');
+			fprintf(fid,'export ISSM_DIR="%s/../"\n',cluster.codepath);
+         fprintf(fid,'source $ISSM_DIR/etc/environment.sh\n');
+			fprintf(fid,'\n');
+			fprintf(fid,'mpirun -n %i %s/%s %s %s %s\n',cluster.nprocs(), cluster.codepath,executable,solution,[cluster.executionpath '/' dirname],modelname);
+			if ~io_gather, %concatenate the output files:
+				fprintf(fid,'cat %s.outbin.* > %s.outbin',modelname,modelname);
+			end
+			fclose(fid);
+
+			%in interactive mode, create a run file, and errlog and outlog file
+			if cluster.interactive
+				fid=fopen([modelname '.run'],'w');
+				fprintf(fid,'mpirun -n %i %s/%s %s %s %s\n',cluster.nprocs(), cluster.codepath,executable,solution,[cluster.executionpath '/' dirname],modelname);
+				if ~io_gather, %concatenate the output files:
+					fprintf(fid,'cat %s.outbin.* > %s.outbin',modelname,modelname);
+				end
+				fclose(fid);
+				fid=fopen([modelname '.errlog'],'w');
+				fclose(fid);
+				fid=fopen([modelname '.outlog'],'w');
+				fclose(fid);
+			end
+		end %}}}
+		function UploadQueueJob(cluster,modelname,dirname,filelist) % {{{
+			cluster_defaults.UploadQueueJob(cluster,modelname,dirname,filelist);
+		end %}}}
+		function LaunchQueueJob(cluster,modelname,dirname,filelist,restart,batch) % {{{
+			cluster_defaults.LaunchQueueJobSbatch(cluster,modelname,dirname,filelist,restart,batch, 2);
+		end %}}}
+		function Download(cluster,dirname,filelist) % {{{
+			cluster_defaults.Download(cluster,dirname,filelist);
+		end %}}}
+	end
+end
