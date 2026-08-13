e152e7404e3e7a200e3c3756ba89c80207a13693
MathieuMorlighem <mathieu.morlighem@gmail.com>
2026-08-13 12:40:18 +0200

    CHG: use same behavior for Schoof as C++ code

--- src/m/mech/basalstress.m
+++ src/m/mech/basalstress.m
@@ -1,18 +1,21 @@
 function [bx by b]=basalstress(md)
-	% BASALSTRESS - compute basal stress from friction law and geometric information. 
-	% Computes basal stress from basal sliding parametrization in md.friction and geometry and ice velocity in md.initialization. Follows the basal stress definition in "src/c/classes/Loads/Friction.cpp", lines 1102-1136. 
-	%
-	% USEAGE:
-	%      b         = basalstress(md); % one argout returns the scalar magnitude				
-	%      [bx by b] = basalstress(md); % multiple argout returns the horizontal vector components
-	% INPUT:
-	%   md		ISSM model from which to take md.friction and md.initialization.
-	% OUTPUT:
-	%   bx		x component of basal stress
-	%   by		y component of basal stress
-	%   b			scalar magnitude of basal stress
-	%
-	%   See also: EFFECTIVEPRESSURE, PLOT_BASALDRAG
+%BASALSTRESS - compute basal stress from friction law and geometric information. 
+%
+%   Computes basal stress from basal sliding parametrization in md.friction and
+%   geometry and ice velocity in md.initialization. Follows the basal stress
+%   definition in "src/c/classes/Loads/Friction.cpp", lines 1102-1136. 
+%
+% USAGE:
+%      b         = basalstress(md); % one argout returns the scalar magnitude				
+%      [bx by b] = basalstress(md); % multiple argout returns the horizontal vector components
+% INPUT:
+%   md		ISSM model from which to take md.friction and md.initialization.
+% OUTPUT:
+%   bx		x component of basal stress
+%   by		y component of basal stress
+%   b			scalar magnitude of basal stress
+%
+%   See also: EFFECTIVEPRESSURE, PLOT_BASALDRAG
 
 	% compute sliding velocity
 	ub  = sqrt(md.initialization.vx.^2+md.initialization.vy.^2)/md.constants.yts; % horizontal vel (m/s)
@@ -24,6 +27,7 @@ function [bx by b]=basalstress(md)
 		case 'friction'
 			% calculate effective pressure using coupling definition in md.friction
 			N = effectivepressure(md); % effective pressure (Pa)
+			any(isnan(N))
 
 			% compute exponents
 			s=averaging(md,1./md.friction.p,0);
@@ -34,11 +38,6 @@ function [bx by b]=basalstress(md)
 		case 'frictionschoof'
 			% calculate effective pressure using coupling definition in md.friction
 			N = effectivepressure(md); % effective pressure (Pa)
-			if any(N < 0)
-				%NOTE: Negative values of effective pressure N return a complex number in alpha2. Treated here with minimum threshold.
-				warning('Find effective pressure value N < 0. Enforcing minimum effective pressure of N_min = 0.1');
-				N = max(N, 0.1);
-			end
 
 			% compute parameters
 			m=averaging(md,md.friction.m,0);
@@ -46,6 +45,8 @@ function [bx by b]=basalstress(md)
 			Cmax=averaging(md,md.friction.Cmax,0);
 
 			alpha2 = (C.^2 .* ub.^(m-1))./(1 + (C.^2./(Cmax.*N)).^(1./m).*ub).^(m);
+			pos = find(ub<1e-10 | N<=0.);
+			alpha2(pos) = 0;
 
 		case 'frictionweertman'
 			m = averaging(md,md.friction.m,0);
--- src/m/mech/basalstress.py
+++ src/m/mech/basalstress.py
@@ -7,8 +7,11 @@ import numpy as np
 
 def basalstress(md):
     '''
-    BASALSTRESS - compute basal stress from friction law and geometric information. 
-    Computes basal stress from basal sliding parametrization in md.friction and geometry and ice velocity in md.initialization. Follows the basal stress definition in "src/c/classes/Loads/Friction.cpp", lines 1102-1136. 
+    BASALSTRESS - compute basal stress from friction law and geometric information.
+
+    Computes basal stress from basal sliding parametrization in md.friction and
+    geometry and ice velocity in md.initialization. Follows the basal stress
+    definition in "src/c/classes/Loads/Friction.cpp", lines 1102-1136.
 
     Usage:
         bx, by, b=basalstress(md)
@@ -38,6 +41,7 @@ def basalstress(md):
     if isinstance(md.friction,friction):
         # calculate effective pressure using coupling definition in md.friction
         N = effectivepressure(md) # effective pressure (Pa)
+        print(np.any(np.isnan(N)))
 
         # compute exponents
         s=averaging(md,1/md.friction.p,0)
@@ -53,17 +57,14 @@ def basalstress(md):
         # calculate effective pressure using coupling definition in md.friction
         N = effectivepressure(md) # effective pressure (Pa)
 
-        if np.any(N<=0):
-            #NOTE: Negative values of effective pressure N return a complex number in alpha2. Treated here with minimum threshold.
-            warnings.warn('Find effective pressure value N < 0. Enforcing minimum effective pressure of N_min = 0.1');
-            N = np.maximum(N,0.1)
-
         # compute parameters
         m=averaging(md,md.friction.m,0)
         C=averaging(md,md.friction.C,0)
         Cmax=averaging(md,md.friction.Cmax,0)
 
         alpha2 = (C**2 * ub**(m-1))/(1 + (C**2/(Cmax*N))**(1/m)*ub)**m
+        pos = np.where((ub < 1e-10) | (N <= 0.))[0]
+        alpha2[pos] = 0
 
     elif isinstance(md.friction,frictionweertman):
         m = averaging(md,md.friction.m,0.0)
